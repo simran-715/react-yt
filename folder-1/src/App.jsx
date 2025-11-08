514947b3d8b0fe2e1139a765ae7e289df8ceb7a4
simran-715 <simran16990@gmail.com>
2025-11-08 12:42:59 +0530

    first commit

---
+++ folder-1/src/App.jsx
@@ -0,0 +1,16 @@
+const App=  () => {
+  return (
+    <>
+      <div id="family">
+        <h1>This is a Family Tree</h1>
+        <h4>Mom</h4>
+        <h4>Dad</h4>
+        <h4>Brother</h4>
+        <h4>Sister</h4>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
